0423e643e6412adb008f08fa19296f20bc184de8
Jerico de guzman <rickzx98@gmail.com>
2017-07-17 00:32:27 +0800

    Update index.js

--- src/boundary/index.js
+++ src/boundary/index.js
@@ -1,9 +1,6 @@
-import { ExpressApp, GDSDomainResource } from 'gds-stack';
 import { BookChains, BookResource } from './book/';
 
-const resource = new GDSDomainResource(ExpressApp, 'api');
-new BookResource(resource);
 module.exports = {
     BookChains,
-    BookResource: resource    
+    BookResource
 };
